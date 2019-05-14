ea1530c87dac50fc51153f595275e0324d938d7b
Vitaly Arkhipov <xstream2006@gmail.com>
2019-05-14 03:11:47 +0200

    Section 10

--- src/components/app/imageList/ImageList.js
+++ src/components/app/imageList/ImageList.js
@@ -3,8 +3,6 @@ import React from 'react';
 import ImageCard from './imageCard/ImageCard';
 
 const ImageList = (props) => {
-  console.log(props.images);
-
   const images = props.images.map((image) => {
     return (
       <ImageCard key={image.id} image={image} />
--- src/components/app/imageList/imageCard/ImageCard.js
+++ src/components/app/imageList/imageCard/ImageCard.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 20);
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
-    // className="ui small spaced image"
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef}
+             onLoad={this.setSpans}
+             alt={description}
+             src={urls.regular} />
       </div>
     );
   }
